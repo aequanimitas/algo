d9f97182be3bea8213565ee01ed96068c66dffbe
metalfatigue <htavenido@gmail.com>
2014-07-31 13:02:03 +0800

    a commit just for the sake of committing

--- gcd_euclid.scm
+++ gcd_euclid.scm
@@ -1,7 +1,16 @@
+(define (message x)
+  (newline)
+  (display x))
+                  
+
 (define (gcd x y)
   (cond ((= x y) x)
-        ((> x y) (gcd (- x y) y))
-        ((< x y) (gcd x (- y x)))
+        ((> x y)
+          (message (- x y))
+          (gcd (- x y) y))
+        ((< x y) 
+          (message (- y x))
+          (gcd x (- y x)))
         (else 0)))
 
-(gcd 3 4)
+(gcd 111111 1234567)
