5964642d768b7f1d7d1fa7948d2fc6df3a8350df
metalfatigue <htavenido@gmail.com>
2014-08-03 23:54:21 +0800

    partial implementation of finding peak

---
+++ peak.js
@@ -0,0 +1,9 @@
+function peak(arr) {
+  return arr.length === 3 ? (function() {
+    if (arr[0] >== arr[1] && arr[0] >== arr[2]) {
+      return arr[0]
+    } 
+  })() : arr.length === 2 ? (function()
+}
+
+peak([0,1])
