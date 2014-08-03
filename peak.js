13227c365ef31b2388976a3835dc6813787ec137
metalfatigue <htavenido@gmail.com>
2014-08-04 01:44:32 +0800

    DAC strategy used, need to catch if already comparing just two elements

--- peak.js
+++ peak.js
@@ -1,7 +1,23 @@
 function peak(arr) {
-  return arr.length === 3 ? (function() {
-   
-  })() : arr.length === 2 ? (function(){})() : arr[0];
+  if (arr.length === 2) {
+    return arr[0] > arr[1] ? arr[0] : arr[1];
+  }
+  var mid = parseInt(arr.length / 2);
+  if (arr[mid] < arr[mid - 1]) {
+    return peak(arr.slice(0, mid-1))
+  } else if (arr[mid] < arr[mid + 1]) { 
+    return peak(arr.slice(mid, arr.len));
+  } else {
+    return arr[0]
+  }
 }
 
-console.log(peak([]));
+var lolarr = [];
+
+for (var x = 0; x < 1000000; x += 1) {
+  lolarr.push(x);
+}
+var starttime = +Date.now();
+console.log(peak(lolarr));
+var endtime = +Date.now();
+console.log(endtime - starttime);
