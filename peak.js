b6b5c8729a95f6b5c7b0f5b05f22a9eecbda1bc5
metalfatigue <htavenido@gmail.com>
2014-08-07 15:31:11 +0800

    added time logger for wrapper

--- peak.js
+++ peak.js
@@ -1,17 +1,30 @@
-function peak(arr) {
-  if (arr.length === 2) {
-    return arr[0] > arr[1] ? arr[0] : arr[1];
-  }
-  var mid = parseInt(arr.length / 2);
-  if (arr[mid] < arr[mid - 1]) {
-    return peak(arr.slice(0, mid - 1))
-  } else if (arr[mid] < arr[mid + 1]) { 
-    return peak(arr.slice(mid, arr.len));
-  } else {
-    return arr[0]
-  }
+var substruct_size_limit = 2,
+      left_side = 0,
+      right_side = 1,
+      operation_counter = 0;
+
+     
+function peak(arr){
+  return finder(arr);
 }
 
+function finder(arr) {
+  operation_counter += 1;
+  var mid = arr.length / 2,
+      mid_to_the_left = mid - 1,
+      mid_to_the_right = mid + 1;
+    if (arr.length === substruct_size_limit) {
+      return arr[left_side] > arr[right_side] ? arr[left_side] : arr[right_side];
+    }
+    var mid = parseInt(arr.length / 2);
+    if (arr[mid] < arr[mid - 1]) {
+      return finder(arr.slice(0, mid - 1))
+    } else if (arr[mid] < arr[mid + 1]) { 
+      return finder(arr.slice(mid, arr.len));
+    } else {
+      return arr[left_side]
+    }
+}
 var lolarr = [];
 
 for (var x = 0; x < 1000000; x += 1) {
@@ -20,4 +33,16 @@ for (var x = 0; x < 1000000; x += 1) {
 var starttime = +Date.now();
 console.log(peak(lolarr));
 var endtime = +Date.now();
-console.log(endtime - starttime);
+console.log('Time took: ' + ((endtime - starttime) / 100) + ' seconds');
+console.log('Array sorted with length of: ' + lolarr.length);
+
+
+console.log("Operation Counter: " + operation_counter);
+operation_counter = 0;
+console.log("Directly call finder fn");
+var new_starttime = +Date.now();
+console.log(finder(lolarr))
+var new_endtime = +Date.now();
+
+console.log("Operation Counter: " + operation_counter);
+console.log('Time took: ' + ((new_endtime - new_starttime) / 100) + ' seconds');
