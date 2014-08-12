aa50e54ad42ef0a45147bf266fc7c61d4dacd5aa
metalfatigue <htavenido@gmail.com>
2014-08-12 09:07:22 +0800

    placeholder for py code, js was just lacking the info I want to see

---
+++ peak.py
@@ -0,0 +1,16 @@
+arr = []
+rand_arr = []
+peak = 0
+for i in range(1000000):
+  arr.append(i)
+
+def algo_001_brute_force():
+  peak = 0 
+  pass
+
+def algo_003_dac():
+  pass
+
+def algo_001_brute_force():
+  pass
+
