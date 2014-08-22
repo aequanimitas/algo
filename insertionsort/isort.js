c22a913aea6a50e9e35ba3d6b4e61cdaa5e7bb49
metalfatigue <htavenido@gmail.com>
2014-08-22 10:41:34 +0800

    add descending

--- insertionsort/isort.js
+++ insertionsort/isort.js
@@ -1,4 +1,10 @@
-function isort(arr, log) {
+function isort(arr, log, fn_order) {
+  log(fn_order.name);
+  log(fn_order(arr));
+  log("\n");
+}
+
+function ascending(arr) {
   var j, i, k = 0;
   for (var ct = 0; ct < arr.length; ct += 1) {
     j = ct + 1;
@@ -11,9 +17,28 @@ function isort(arr, log) {
       j -= 1;
     }
   }
-  log(arr);
+  return arr;
 }
 
-isort([219,132,99,102,88,103], console.log);
+function descending(arr) {
+  var i,j,k = 0;
+  for (var ct = arr.length; ct >= 0; ct -= 1) {
+    j = ct - 1;
+    i = ct;
+    while ((i >= 0) && (arr[i] > arr[j])) {
+      k = arr[j];
+      arr[j] = arr[i];
+      arr[i] = k;
+      i -= 1;
+      j -= 1;
+    }
+  } 
+  return arr;
+}
+
+console.log([219,132,99,102,88,103]);
+
+isort([219,132,99,102,88,103], console.log, ascending);
+isort([219,132,99,102,88,103], console.log, descending);
 
 
