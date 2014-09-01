42f021dd95ee98b237b95953958d9957b36afe35
metalfatigue <htavenido@gmail.com>
2014-09-01 08:36:05 +0800

    review on recursive and iterative evaluation of fibonacci series

---
+++ 0/fib.scm
@@ -0,0 +1,30 @@
+(define (fib-r n)
+  (newline)
+  (display n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib-r (- n 1))
+                 (fib-r (- n 2))))))
+
+(fib-r 5)
+
+(define (fib-i-02 n)
+  (define (iter x y z)
+    (newline)
+    (display "Current term: ")
+    (display y)
+    (newline)
+    (display "Next term: ")
+    (display x)
+    (newline)
+    (display "Counter: ")
+    (display z)
+    (newline)
+    (if (= z 0)
+        y
+        (iter (+ x y) x (- z 1))))
+  (iter 1 0 n))
+
+; (= (fib-r 9) (fib-i-02 9))
+
+(fib-i-02 999)
